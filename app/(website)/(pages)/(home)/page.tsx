e11c0de37252baf3a4c9ae80a06fb71f7831ba41
Mr-Rafiqul-Islam <rafiqulislamemon05@gmail.com>
2025-01-04 17:23:16 +0600

    App About section done

--- app/(website)/(pages)/(home)/page.tsx
+++ app/(website)/(pages)/(home)/page.tsx
@@ -1,12 +1,14 @@
+import About from "@/components/About";
 import Banner from "@/components/Banner";
 import SearchBar from "@/components/SearchBar";
 
 
 export default function Home() {
   return (
-      <main className=" min-h-screen font-[family-name:var(--font-geist-sans)]">
+      <main className="font-[family-name:var(--font-geist-sans)]">
         <Banner/>
         <SearchBar/>
+        <About/>
         
       </main>
   );
