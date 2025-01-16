ad6a0e00560134b3f7da1865eddab7b852c8603e
Mr-Rafiqul-Islam <rafiqulislamemon05@gmail.com>
2025-01-16 15:32:49 +0600

    Seatlayout Added

--- components/bus-booking/SeatBooking.tsx
+++ components/bus-booking/SeatBooking.tsx
@@ -11,6 +11,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { PiSeatBold } from "react-icons/pi";
 import { GiStarFormation } from "react-icons/gi";
 import { IoShieldCheckmarkOutline } from "react-icons/io5";
+import SeatLayout from "./SeatLayout";
 
 const seats = [
   "A1",
@@ -159,69 +160,15 @@ function SeatBooking({
                     </li>
                   </ul>
                 </div>
-
-                <div className="grid grid-cols-2 gap-10 mt-2">
-                  <div className="grid grid-cols-2 gap-2">
-                    {seats
-                      .filter(
-                        (seat) => seat.includes("1") || seat.includes("2")
-                      )
-                      .map((seat, i) => (
-                        <>
-                          <button
-                            key={i}
-                            disabled={bookedSeats.includes(seat)}
-                            onClick={() => toggleSeat(seat)}
-                            className={`p-2 rounded ${
-                              bookedSeats.includes(seat)
-                                ? "bg-red-500 text-white opacity-50 cursor-not-allowed"
-                                : selectedSeats.includes(seat) &&
-                                  selectedSeats.length <= maxSeats
-                                ? "bg-primary-color text-white"
-                                : "bg-gray-300 text-gray-600"
-                            } ${
-                              selectedSeats.length >= maxSeats &&
-                              !selectedSeats.includes(seat)
-                                ? "opacity-50 cursor-not-allowed"
-                                : ""
-                            }`}
-                          >
-                            {seat}
-                          </button>
-                        </>
-                      ))}
-                  </div>
-                  <div className="grid grid-cols-2 gap-2">
-                    {seats
-                      .filter(
-                        (seat) => seat.includes("3") || seat.includes("4")
-                      )
-                      .map((seat, i) => (
-                        <>
-                          <button
-                            key={i}
-                            disabled={bookedSeats.includes(seat)}
-                            onClick={() => toggleSeat(seat)}
-                            className={`p-2 rounded ${
-                              bookedSeats.includes(seat)
-                                ? "bg-red-500 text-white opacity-50 cursor-not-allowed"
-                                : selectedSeats.includes(seat) &&
-                                  selectedSeats.length <= maxSeats
-                                ? "bg-primary-color text-white"
-                                : "bg-gray-300 text-gray-600"
-                            } ${
-                              selectedSeats.length >= maxSeats &&
-                              !selectedSeats.includes(seat)
-                                ? "opacity-50 cursor-not-allowed"
-                                : ""
-                            }`}
-                          >
-                            {seat}
-                          </button>
-                        </>
-                      ))}
-                  </div>
-                </div>
+                {/* Seat Layout part start  */}
+                <SeatLayout
+                  seats={seats}
+                  bookedSeats={bookedSeats}
+                  selectedSeats={selectedSeats}
+                  toggleSeat={toggleSeat}
+                  maxSeats={maxSeats}
+                />
+                {/* Seat Layout part end  */}
               </div>
               <div className="mt-4">
                 <p className="text-lg font-bold">Total: ৳2000</p>
