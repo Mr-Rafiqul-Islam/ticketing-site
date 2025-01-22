4b5d9ed16ea4690c2b43881dd5df5d060890074c
Mr-Rafiqul-Islam <rafiqulislamemon05@gmail.com>
2025-01-22 17:17:15 +0600

    api set in variable

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,13 @@
 import type { NextConfig } from "next";
 
+const MyApi = process.env.NEXT_PUBLIC_API_URL;
 const nextConfig: NextConfig = {
   /* config options here */
   async rewrites() {
     return [
       {
         source: "/api/:path*", // Frontend API route
-        destination: "https://touch.bytecareltd.com/api/:path*", // Backend API
+        destination: `${MyApi}/api/:path*`, // Backend API
       },
     ];
   },
