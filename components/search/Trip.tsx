e508944e21c04630450ab1016587bf6763bc37b1
Mr-Rafiqul-Islam <rafiqulislamemon05@gmail.com>
2025-03-18 15:58:13 +0600

    ui implement

--- components/search/Trip.tsx
+++ components/search/Trip.tsx
@@ -6,14 +6,16 @@ import { useSearchContext } from "@/utlis/provider/SearchProvider";
 export default function Trip() {
   const { data } = useSearchContext();
   console.log(data, "Context DATA");
+  const trips = data?.trips;
+  console.log(trips, "Trips from context");
   return (
     <div className="grid grid-cols-1 gap-4">
-      <BusCard />
-      <BusCard />
-      <BusCard />
-      <BusCard />
-      <BusCard />
-      <BusCard />
+        {
+            trips?.map((trip: any, index: number) => (
+                <BusCard key={index} />
+            ))
+        }
+      
     </div>
   );
 }
