ba1e68e04623803d0d3b39ace670c57d50385b23
Mr-Rafiqul-Islam <rafiqulislamemon05@gmail.com>
2025-03-18 15:53:56 +0600

    search bus api implement on search page

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -21,13 +21,22 @@ import { Check, ChevronsUpDown } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { useRouter } from "next/navigation";
 import { useFetchLocations } from "@/utlis/hooks/useFetchLocations";
+import { useSearchTrip } from "@/utlis/hooks/useSearchTrip";
+import { format } from "date-fns";
+import { useSearchContext } from "@/utlis/provider/SearchProvider";
 
-
-
+interface searchTripvalues {
+  from_location_id: number;
+  to_location_id: number;
+  date: string;
+}
 function SearchBar() {
+
   const [tripType, setTripType] = useState("oneWay");
   const [from, setFrom] = useState("");
+  const [fromId, setFromId] = useState('');
   const [to, setTo] = useState("");
+  const [toId, setToId] = useState('');
   const [journeyDate, setJourneyDate] = useState<Date | undefined>();
   const [returnDate, setReturnDate] = useState<Date | undefined>();
   const router = useRouter();
@@ -35,7 +44,7 @@ function SearchBar() {
   const { data, error, isLoading } = useFetchLocations();
   const locationArray = data?.locations;
 
-  console.log(locationArray, "lcation from API response");
+  // console.log(locationArray, "lcation from API response");
 
   // const locations = [
   //   { id: 1, value: "dhaka", label: "Dhaka" },
@@ -46,8 +55,9 @@ function SearchBar() {
     value: loc.name.toLowerCase().replace(/\s+/g, ""),
     label: loc.name,
   }));
-  console.log(locations);
-
+  // console.log(locations);
+  const { mutate, status, error: searchError } = useSearchTrip();
+  const { setData } = useSearchContext();
   const handleSearch = () => {
     if (!from || !to || !journeyDate) {
       alert("Please fill in all required fields!");
@@ -55,8 +65,23 @@ function SearchBar() {
     }
     const formattedFrom = from.replace(/\s+/g, "-");
     const formattedTo = to.replace(/\s+/g, "-");
-    const formattedDate = journeyDate.toISOString().split("T")[0];
+    const formattedDate = format(journeyDate, "yyyy-MM-dd"); 
     router.push(`/search/${formattedFrom}/${formattedTo}/${formattedDate}`);
+    
+    mutate({
+      from_location_id: fromId,
+      to_location_id: toId,
+      date: formattedDate,
+    }, {
+      onSuccess: (data) => {
+        // console.log(data, "TRIP DATA");
+        setData(data); // Store data in context
+        return data;
+      },
+      onError: (error) => {
+        console.error(error);
+      },
+    });
   };
 
   return (
@@ -104,7 +129,10 @@ function SearchBar() {
                           {locations?.map((location: any) => (
                             <CommandItem
                               key={location.value}
-                              onSelect={() => setFrom(location.value)}
+                              onSelect={() => {
+                                setFrom(location.value);
+                                setFromId(location.id);
+                              }}
                             >
                               <Check
                                 className={cn(
@@ -147,7 +175,10 @@ function SearchBar() {
                           {locations?.map((location: any) => (
                             <CommandItem
                               key={location.value}
-                              onSelect={() => setTo(location.value)}
+                              onSelect={() => {
+                                setTo(location.value);
+                                setToId(location.id);
+                              }}
                             >
                               <Check
                                 className={cn(
