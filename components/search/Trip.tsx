9491d133df6663b64f4d9566afdc8febee06ce8e
Mr-Rafiqul-Islam <rafiqulislamemon05@gmail.com>
2025-03-19 13:15:58 +0600

    dynamic bus trip design

--- components/search/Trip.tsx
+++ components/search/Trip.tsx
@@ -2,17 +2,18 @@
 import React from "react";
 import BusCard from "./BusCard";
 import { useSearchContext } from "@/utlis/provider/SearchProvider";
+import { Trip as BusTrip } from "@/types";
 
 export default function Trip() {
   const { data } = useSearchContext();
-  console.log(data, "Context DATA");
+  // console.log(data, "Context DATA");
   const trips = data?.trips;
-  console.log(trips, "Trips from context");
+  // console.log(trips, "Trips from context");
   return (
     <div className="grid grid-cols-1 gap-4">
         {
-            trips?.map((trip: any, index: number) => (
-                <BusCard key={index} />
+            trips?.map((trip: BusTrip, index: number) => (
+                <BusCard key={index} trip={trip}/>
             ))
         }
       
