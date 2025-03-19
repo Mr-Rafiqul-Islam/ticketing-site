efe18aaee2ab7633072004023579a7835caa61e4
Mr-Rafiqul-Islam <rafiqulislamemon05@gmail.com>
2025-03-19 13:15:18 +0600

    trip type added

--- types/index.ts
+++ types/index.ts
@@ -11,3 +11,149 @@ export type userData = {
   created_at: "string (datetime)";
   updated_at: "string (datetime)";
 };
+
+export type Division = {
+  id: number;
+  name: string;
+  status: number;
+  created_at: string;
+  updated_at: string;
+};
+
+export type District = {
+  id: number;
+  division_id: number;
+  name: string;
+  status: number;
+  created_at: string;
+  updated_at: string;
+  division: Division;
+};
+
+export type Location = {
+  id: number;
+  district_id: number;
+  name: string;
+  status: number;
+  created_at: string;
+  updated_at: string;
+  district: District;
+};
+
+export type Counter = {
+  id: number;
+  company_id: number;
+  name: string;
+  counter_no: string | null;
+  location_id: number;
+  status: string;
+  created_at: string;
+  updated_at: string;
+};
+
+export type Employee = {
+  id: number;
+  company_id: number;
+  department: string;
+  name: string;
+  email: string;
+  phone: string;
+  address: string;
+  document: string | null;
+  nid: string;
+  status: string;
+  created_at: string;
+  updated_at: string;
+};
+
+export type Route = {
+  id: number;
+  company_id: number;
+  from_location_id: string;
+  to_location_id: string;
+  start_counter_id: number;
+  end_counter_id: number;
+  via_counters_id: string;
+  route_manager_id: number;
+  checkers_id: string[];
+  document: string;
+  status: string;
+  created_at: string;
+  updated_at: string;
+  from_location: Location;
+  to_location: Location;
+  start_counter: Counter;
+  end_counter: Counter;
+  route_manager: Employee;
+};
+
+export type Vehicle = {
+  id: number;
+  company_id: number;
+  owner_id: number;
+  type_id: number;
+  category: string;
+  name: string;
+  vehicle_no: string;
+  engin_no: string;
+  chest_no: string;
+  total_seat: number;
+  amenities_id: string[];
+  document: string | null;
+  is_booked: string;
+  current_location_id: number;
+  status: number;
+  created_at: string;
+  updated_at: string;
+  owner: Employee;
+  type: {
+    id: number;
+    company_id: number;
+    name: string;
+    status: number;
+    created_at: string;
+    updated_at: string;
+  };
+};
+
+export type Company = {
+  id: number;
+  name: string;
+  email: string;
+  email_verified_at: string | null;
+  phone: string | null;
+  verification_code: string | null;
+  reset_password_token: string | null;
+  reset_password_token_created_at: string | null;
+  image: string | null;
+  status: number;
+  is_registration_by: string | null;
+  created_at: string;
+  updated_at: string;
+};
+
+export type Trip = {
+  id: number;
+  company_id: number;
+  route_id: number;
+  vehicle_id: number;
+  driver_id: number;
+  supervisor_id: number;
+  helper_id: number;
+  start_date: string;
+  end_date: string;
+  start_time: string;
+  end_time: string;
+  reporting_time: string;
+  total_route_cost: string;
+  ticket_price: string;
+  trip_status: number;
+  status: string;
+  created_at: string;
+  updated_at: string;
+  route: Route;
+  vehicle: Vehicle;
+  company: Company;
+  driver: Employee;
+  supervisor: Employee;
+};
